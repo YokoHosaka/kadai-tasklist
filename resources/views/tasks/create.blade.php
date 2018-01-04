6af8ba7b5d1c45d01294a1cb04d39be60c4c0f0c
Yotaro.Yone <lemon25405@gmail.com>
2018-01-04 14:56:59 +0000

    Bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,18 +1,28 @@
 @extends('layouts.app')
 @section('content')
 
-    <h1>タスクの新規作成ページ</h1>
+    <h1>タスクの新規登録</h1>
     
-    {!! Form::model( $task, ['route' => 'tasks.store']) !!}
+    <div class="row">
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6"> 
     
-         {!! Form::label('status', '状態:') !!}
-         {!! Form::text('status') !!}
-         
-         {!! Form::label('content', 'タスク：') !!}
-         {!! Form::text('content') !!}
-         
-         {!! Form::submit('登録する') !!}
-         
-    {!! Form::close() !!}
+            {!! Form::model( $task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                     {!! Form::label('status', 'ステータス：') !!}
+                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                 
+                <div class="form-group">
+                     {!! Form::label('content', 'タスク：') !!}
+                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                 
+                {!! Form::submit('登録する', ['class' => 'btn btn-primary']) !!}
+                 
+            {!! Form::close() !!}
+            
+        </div>
+    </div>
     
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,14 +1,28 @@
 @extends('layouts.app')
 @section('content')
     <h1>id = {{ $task -> id }} のタスク詳細ページ</h1>
-        
-        <p>{{ $task => status }} </p>
-        <p>{{ $task -> content }} </p>
+        <table class="table table-bordered">
+            <tr>
+                <th>id</th>
+                <td>{{ $task->id }}</td>
+            </tr>
+            
+            <tr>
+                <th>ステータス</th>
+                <td>{{ $task -> status }} </td>
+            </tr>
+            
+            <tr>
+                <th>タスク</th>
+                <td>{{ $task -> content }}</td>
+            </tr>
+         </table>
     
-    {!! link_to_route('tasks.edit', 'このタスクの内容を更新', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このタスクの内容を変更', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+    {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    
     {!! Form::close() !!}
     
 @endsection
